b724ed049f98e69919000089ec7f569b12f01d63
Matheus Moura Gorchinsky <62040651+MatGorky@users.noreply.github.com>
2022-02-07 22:24:51 -0300

    Title screen inicial

---
+++ Assets/Scripts/TitleScreen/exit.gd
@@ -0,0 +1,6 @@
+extends Node
+
+
+func _on_Exit_button_down():
+	get_tree().quit()
+
---
+++ Assets/Scripts/TitleScreen/new_game.gd
@@ -0,0 +1,4 @@
+extends Node
+
+func _on_NewGame_button_down():
+	get_tree().change_scene("res://Scenes/Node2DCatCafe.tscn")
