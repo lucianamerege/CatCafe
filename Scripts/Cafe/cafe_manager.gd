517cca6952ed86525410b6a18473904cb4be9bc6
Matheus Moura Gorchinsky <62040651+MatGorky@users.noreply.github.com>
2022-02-13 20:55:06 -0300

    Marketing Update
    Nossas táticas de marketing funcionaram e começamos a atrair clientes uhuuu ^^
    Embora eles pareçam tapetes -w-

--- Scripts/Cafe/cafe_manager.gd
+++ Scripts/Cafe/cafe_manager.gd
@@ -8,13 +8,16 @@ var time = 0.0
 var aux_time = 0.0
 var rng = RandomNumberGenerator.new()
 var client_counter = 0
-var table_list = 0
+var client_limit = 3
+var table_list = []
 
 const CLIENT_MIN_INTERVAL = 3.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	create_tables()
+	print(table_list[2].position)
+	print(table_list[2].occupied)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -22,12 +25,17 @@ func _process(delta):
 	time += delta
 	if time > CLIENT_MIN_INTERVAL:
 		aux_time += delta
-		if aux_time > 1:
+		if aux_time > 1 and client_counter < client_limit:
 			try_spawn()
 			aux_time -= 1.0
 
+func create_tables():
+	table_list.append(Table.new(Vector2(500,285)))
+	table_list.append(Table.new(Vector2(335,285)))
+	table_list.append(Table.new(Vector2(145,285)))
+
 func try_spawn():
-	var chance = time/2
+	var chance = time/2 * (client_limit - client_counter) * 1.5/client_limit
 	print("tento spawnar")
 	var value = rng.randi_range(1,100)
 	if value < chance:
@@ -40,6 +48,7 @@ func spawn_client():
 	client_counter += 1
 	var client_path = "res://Scenes/Entity/Client.tscn"
 	var client = load(client_path).instance()
-	client.global_position = self.position + Vector2(16,8)
+	client.global_position = self.position + Vector2(415,180)
 	print("cliente " + str(client_counter) + " spawnou em" + str(client.global_position))
+	add_child(client)
 	
