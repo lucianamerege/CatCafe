66391bb27aa4e50998696de24d58699165ca3d02
Luciana <52220617+lucianamerege@users.noreply.github.com>
2022-02-15 00:49:45 -0300

    consertando-merge

--- Scenes/NovoJogo.gd
+++
@@ -1,4 +0,0 @@
-extends TextureButton
-
-func _on_NovoJogo_button_down():
-	get_tree().change_scene("res://Scenes/Cafe.tscn")
---
+++ Scripts/TitleScreen/NovoJogo.gd
@@ -0,0 +1,4 @@
+extends TextureButton
+
+func _on_NovoJogo_button_down():
+	get_tree().change_scene("res://Scenes/Level/Cafe.tscn")
