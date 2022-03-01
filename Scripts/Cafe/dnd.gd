49a2bf9cedb2a11a07927e97445f5bca5d5a090b
Julia Pohlmann jubspohlmann <juliapohlmann@dcc.ufrj.br>
2022-03-01 15:20:51 -0300

    tentativas

--- Scripts/Cafe/dnd.gd
+++ Scripts/Cafe/dnd.gd
@@ -1,36 +1,25 @@
-extends Node
-class_name Card
+extends KinematicBody2D
 
-var carisma
-var tecnica
-var stamina
-var nome
 var dragging = false
 
 signal dragsignal;
 
-
-#construtor
-func _init(car, tec, sta, name):
-	nome = name
-	carisma = car
-	tecnica = tec
-	stamina = sta
-	
 func _ready():
-	print("INICIO")
 	connect("dragsignal",self,"_set_drag_pc")
+	
 	
 func _process(delta):
 	if dragging:
 		var mousepos = get_viewport().get_mouse_position()
 		self.position = Vector2(mousepos.x, mousepos.y)
 
+		
+
 func _set_drag_pc():
 	dragging=!dragging
 
+
 func _on_KinematicBody2D_input_event(viewport, event, shape_idx):
-	print("tentativa de arrastar", event, shape_idx)
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT and event.pressed:
 			emit_signal("dragsignal")
@@ -39,6 +28,4 @@ func _on_KinematicBody2D_input_event(viewport, event, shape_idx):
 	elif event is InputEventScreenTouch:
 		if event.pressed and event.get_index() == 0:
 			self.position = event.get_position()
-
-#func _on_KinematicBody2D_input_event(viewport, event, shape_idx):
-#	pass # Replace with function body.
+		
